4c0fd7d9c82539ed0dba77b8f3c8956ef530ccca
Valrich12 <varica12@outlook.com>
2023-03-13 12:37:27 -0600

    String Arreglado

--- PRACTICA2/posTagging.py
+++ PRACTICA2/posTagging.py
@@ -8,9 +8,11 @@ def separate_news(file_name):
     # Lee el corpus original del archivo de entrada y lo pasa a una DataFrame
     df = pd.read_csv(file_name, sep='&&&&&&&&', dtype=str,engine='python')
     news = df.drop(df.columns[[0,1,3]],axis=1)
-    print(news)
+    news.columns[[0]].str.strip()
     news = news.to_string(index=False)
-    
+    news = news.strip()
+    news = news.replace("   ","")
+    print(news)
     f = open('test.txt','w',encoding='utf-8')
     f.write(news)
     f.close
@@ -24,8 +26,6 @@ def separate_news(file_name):
 
 if __name__ == "__main__":
     news = separate_news('corpus_noticias.txt')
-    print("ESTO ES UNA PRUEBA")
-    print("ESTO ES OTRA PRUEBA")
 
 # cadena = "Juan estaba corriendo por el pasillo de la escuela superior de cómputo. "
 # # ~ cadena = "Los perros ladraron la otra noche a unos coches rojos que pasaron por la calle."
